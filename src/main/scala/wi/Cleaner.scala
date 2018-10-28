899ac7ae1cef16ac0a5043474b1427743ef881e8
granelj <joris.granel@etu.umontpellier.fr>
2018-10-28 13:36:35 +0100

    ADD : Function to prepare de the dataframe with string indexer

--- src/main/scala/wi/Cleaner.scala
+++ src/main/scala/wi/Cleaner.scala
@@ -61,7 +61,7 @@ object Cleaner {
   }
 
   /**
-    Return a new dataframe with the given column filled with the given value
+    Return a new dataframe with the given column filled with the given String value
     @param dataFrame DataFrame to change 
     @param columnName name of the columns to change
     @param value String with which to fill
@@ -71,14 +71,86 @@ object Cleaner {
     return ndf
   }
 
-  def indextype(dataFrame: DataFrame): DataFrame = {
-    val ndf = fillWithString(dataFrame, "type", "null")
+  /**
+    Return a new dataframe with the given column filled with the given Double value
+    @param dataFrame DataFrame to change 
+    @param columnName name of the columns to change
+    @param value Double with which to fill
+  */
+  def fillWithDouble(dataFrame: DataFrame, columnName : String, value : Double): DataFrame = {
+    val ndf = dataFrame.na.fill(value, Seq(columnName))
+    return ndf
+  }
+
+  /**
+    Return a new dataframe with the given column filled with the given Boolean value
+    @param dataFrame DataFrame to change 
+    @param columnName name of the columns to change
+    @param value Boolean with which to fill
+  */
+  def fillWithBoolean(dataFrame: DataFrame, columnName : String, value : Boolean): DataFrame = {
+    val ndf = dataFrame.na.fill(value, Seq(columnName))
+    return ndf
+  }
+
+  /**
+    Return a new dataframe with indexed field
+    @param dataFrame DataFrame to change 
+    @param column name of the column to index
+  */
+  def toIndex(dataFrame: DataFrame, column: String): DataFrame = {
     val indexer = new StringIndexer()
-      .setInputCol("type")
-      .setOutputCol("indextype")
-    val indexed = indexer.fit(ndf).transform(ndf)
-    indexed.drop("type")
-    return indexed
+      .setInputCol(column)
+      .setOutputCol("index" + column)
+    val indexed = indexer.fit(dataFrame).transform(dataFrame)
+    val nindexed = indexed.drop(column)
+    return nindexed
+  }
+
+  def toVector(dataFrame: DataFrame, column: String): DataFrame = {
+    new VectorAssembler()
+      .setInputCols(Array(column))
+      .setOutputCol("vector" + column)
+      .transform(dataFrame)
+  }
+
+  def fillDF(dataFrame: DataFrame): DataFrame = {
+    var filldf = dataFrame
+    filldf = fillWithString(filldf, Column.APP_OR_SITE.toString, "null")
+    filldf = fillWithDouble(filldf, Column.BID_FLOOR.toString, 0.0)
+    filldf = fillWithString(filldf, Column.CITY.toString, "null")
+    filldf = fillWithString(filldf, Column.EXCHANGE.toString, "null")
+    filldf = fillWithString(filldf, Column.INTERESTS.toString, "null")
+    filldf = fillWithBoolean(filldf, Column.LABEL.toString, false)
+    filldf = fillWithString(filldf, Column.MEDIA.toString, "null")
+    filldf = fillWithString(filldf, Column.NETWORK.toString, "null")
+    filldf = fillWithString(filldf, Column.OS.toString, "null")
+    filldf = os(filldf)
+    filldf = fillWithString(filldf, Column.PUBLISHER.toString, "null")
+    filldf = fillWithString(filldf, Column.TYPE.toString, "null")
+    filldf = fillWithString(filldf, Column.USER.toString, "null")
+    return filldf
   }
   
+  def stringIndexerDF(dataFrame: DataFrame): DataFrame = {
+    var indexedDF = dataFrame
+    indexedDF = toIndex(indexedDF, Column.APP_OR_SITE.toString)
+    indexedDF = toIndex(indexedDF, Column.CITY.toString)
+    indexedDF = toIndex(indexedDF, Column.EXCHANGE.toString)
+    indexedDF = toIndex(indexedDF, Column.MEDIA.toString)
+    indexedDF = toIndex(indexedDF, Column.NETWORK.toString)
+    indexedDF = toIndex(indexedDF, Column.OS.toString)
+    indexedDF = toIndex(indexedDF, Column.PUBLISHER.toString)
+    indexedDF = toIndex(indexedDF, Column.TYPE.toString)
+    return indexedDF
+  }
+
+  def prepareDF(dataFrame: DataFrame): DataFrame ={
+    var ndf = dataFrame
+    ndf = fillDF(ndf)
+    ndf = stringIndexerDF(ndf)
+    ndf = impid(ndf)
+    ndf = interests(ndf)
+    return ndf
+  }
 }
--- src/main/scala/wi/Main.scala
+++ src/main/scala/wi/Main.scala
@@ -10,11 +10,7 @@ object Main extends App {
   val file = "data-students.json" // Should be some file on your system
   val spark = SparkSession.builder.appName("Wi_App").config("spark.master", "local").getOrCreate()
   val dfJson = spark.read.json(file)
-  //dfJson.printSchema()
-  //dfJson.createOrReplaceTempView("df")
-  //spark.sql("SELECT bidfloor FROM df").show()
-  //val nDataFrame = Cleaner.impid(dfJson)
-  //val nnDataFrame = Cleaner.os(nDataFrame)
-  Cleaner.indextype(dfJson).printSchema()
+  val df = Cleaner.prepareDF(dfJson)
+  df.show()
   spark.stop()
 }
